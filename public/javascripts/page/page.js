675fed3aaba46ea7df31e0d39e5da5617f7d45d2
Marshal Wu <marshal.wu@gmail.com>
2012-09-08 10:20:49 +0800

    程序最基本架子

---
+++ public/javascripts/page/page.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: marshal
+ * Date: 12-9-7
+ * Time: 下午5:53
+ * To change this template use File | Settings | File Templates.
+ */
+
+var marshal = {//创建和初始化marshal命名空间
+    init:function () {
+    }
+};
+$(marshal.init);
